18fcdd87aac4f304e5ebef77e0bf76d87d269d8b
rafaelgdgs <rafagsique@hotmail.com>
2025-04-09 16:19:03 -0300

    feat: definition of iris data types and basic functions

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+fn read_file(path: &str) -> String {
+    std::fs::read_to_string(path).expect("Couldn't read the file.")
+}
+
+fn load_data(str: String) -> Vec<Line> {
+    let mut data: Vec<Line> = vec![];
+    for line in str.split('\n') {
+        let mut parts = line.split(';');
+        let sepal_length = parts
+            .next()
+            .expect("Failed to get Sepal Length")
+            .parse::<f32>()
+            .expect("Failed to define sepal_length");
+        let sepal_width = parts
+            .next()
+            .expect("Failed to get Sepal Width")
+            .parse::<f32>()
+            .expect("Failed to define sepal_width");
+        let petal_length = parts
+            .next()
+            .expect("Failed to get Petal Length")
+            .parse::<f32>()
+            .expect("Failed to define petal_length");
+        let petal_width = parts
+            .next()
+            .expect("Failed to get Petal Width")
+            .parse::<f32>()
+            .expect("Failed to define petal_width");
+        let class = classify_class(parts.next().expect("Failed to get Petal Width"))
+            .expect("Couldn't set class");
+        let current: Line = Line {
+            sepal_length,
+            sepal_width,
+            petal_length,
+            petal_width,
+            class,
+        };
+        data.push(current);
+    }
+    data
+}
+
+fn classify_class(str: &str) -> Option<Classes> {
+    match str {
+        "Iris-virginica" => Some(Classes::Virginica),
+        "Iris-versicolor" => Some(Classes::Versicolour),
+        "Iris-setosa" => Some(Classes::Setosa),
+        _ => None,
+    }
+}
+
+enum Classes {
+    Setosa,
+    Versicolour,
+    Virginica,
+}
+
+struct Line {
+    sepal_length: f32,
+    sepal_width: f32,
+    petal_length: f32,
+    petal_width: f32,
+    class: Classes,
+}
+
+fn main() {
+    let model_data = read_file("/home/jhin/Downloads/iris/iris.data");
+    println!("Hello, world!");
+}
